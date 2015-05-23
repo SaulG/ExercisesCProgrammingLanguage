9b24fe10266b8ee5cf0e33a73e585672699baeba
Saul Gausin <saul.gausin@gmail.com>
2015-05-23 15:22:20 -0500

    Add program testing pointers to double array of chars

--- testingDoubleArrayPointers.c
+++ testingDoubleArrayPointers.c
@@ -2,21 +2,38 @@
 
 int main() {
   int numbers[10][10];
-  int (*pointer)[10][10];
-  pointer = &numbers;
+  int (*numbers_p)[10][10];
+  numbers_p = &numbers;
   int numero = 2;
   for (int i = 0; i< 10; i++) {
     for (int j = 0; j< 10; j++) {
       printf("1 --- imprimiendo i: %d j:%d r: %d\n", i, j, numero * i * j);
-      (*pointer)[i][j] = numero * i * j;
+      (*numbers_p)[i][j] = numero * i * j;
     }
   }  
 
   for (int i = 0; i< 10; i++) {
     for (int j = 0; j< 10; j++) {
-      printf("2 --- imprimiendo i: %d j:%d r: %d\n", i, j, (*pointer)[i][j]);
+      printf("2 --- imprimiendo i: %d j:%d r: %d\n", i, j, (*numbers_p)[i][j]);
     }
   }
+
+  char characters[10][10];
+  char (*characters_p)[10][10];
+  characters_p = &characters;
+  for (int i = 0; i< 10; i++) {
+    for (int j = 0; j< 9; j++) {
+      printf("1 --- imprimiendo i: %d j:%d r: %c\n", i, j, 'a');
+      (*characters_p)[i][j] = 'a';
+    }
+    (*characters_p)[i][10] = '\0';
+  }  
+
+  for (int i = 0; i< 10; i++) {
+    printf("2 --- imprimiendo i: %d r: %s\n", i, (*characters_p)[i]);
+  }
+
+
 
   return 1;
 }
